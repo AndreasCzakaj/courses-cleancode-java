bb250d94cd8cec682b8a9d86b6f21725bdedef9a
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2024-11-30 15:02:25 +0100

    add PersonTest

--- dirty/src/main/java/eu/binarystars/cleancode/naming/Person.java
+++ dirty/src/main/java/eu/binarystars/cleancode/naming/Person.java
@@ -1,12 +1,5 @@
 package eu.binarystars.cleancode.naming;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Person {
     String coreFirstName;
     String coreLastName;
