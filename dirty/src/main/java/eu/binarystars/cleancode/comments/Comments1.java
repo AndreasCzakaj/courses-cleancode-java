0248e9208dbfa6bd6511e845672165cfc1db9603
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2024-11-30 14:43:32 +0100

    revert to dirty state

--- dirty/src/main/java/eu/binarystars/cleancode/comments/Comments1.java
+++ dirty/src/main/java/eu/binarystars/cleancode/comments/Comments1.java
@@ -2,21 +2,12 @@ package eu.binarystars.cleancode.comments;
 
 public class Comments1 {
     void init() {
-        var port = getThePortFromEnvOrUseDefault();
-
-        startServer(port);
-    }
-
-    private static void startServer(final int port) {
-        // start server on given port
-        System.out.println(port);
-    }
-
-    private static int getThePortFromEnvOrUseDefault() {
         // get the port from env or use default
         String portAsString = System.getenv("port");
         final int defaultPort = 8080;
         int port = portAsString == null ? defaultPort : Integer.parseInt(portAsString);
-        return port;
+
+        // start server on given port
+        System.out.println(port);
     }
 }
