ce8c7a5fd6d19a398423207eafb080b51ad67c5b
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-07-30 17:01:45 +0200

    fix example

--- examples/src/main/java/eu/binarystars/cleancode/naming/Labeler2.java
+++ examples/src/main/java/eu/binarystars/cleancode/naming/Labeler2.java
@@ -2,7 +2,7 @@ package eu.binarystars.cleancode.naming;
 
 public class Labeler2 {
     private ILabelPrinter printer;
-    private String[] a;
+    private String[] a = new String[10];
     public void prAL() {
         for (int i = 0; i < a.length; i++) {
             printer.printLine(a[i]);
